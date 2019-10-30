77a2e0c5615189a2cb887fd990ece3dc8a28e302
Antonio Raimundo <antoniorrm@Mac-mini-de-Eduardo.local>
2019-10-30 15:43:10 -0300

    adonis seed add in project

---
+++ database/seeds/UserSeeder.js
@@ -0,0 +1,27 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| UserSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use('Factory')
+const Database = use('Database')
+
+class UserSeeder {
+  async run () {
+    await Factory
+          .model('App/Models/User')
+          .createMany(5)
+  }
+}
+
+module.exports = UserSeeder
+
+
